6c85235b6d31fdf7cf0609dc41a9a4f32e1a7134
We1l6.f <we1l6.f@gmail.com>
2025-06-14 19:46:07 +0300

    style: change style

--- src/userInterface/userInterface.cpp
+++ src/userInterface/userInterface.cpp
@@ -1,46 +1,29 @@
 #include "userInterface.h"
 
-
-
-UserInterface::UserInterface(RectanglePro& rect)
-	: m_rect(rect), m_text("")
-{
-	m_text.position = {
-		.x = m_rect.x + (m_rect.width / 2.0f),
-		.y = m_rect.y + (m_rect.height / 2.0f)
-	};
-	m_text.rotation = m_rect.rotation;
-	m_text.CenterOrigin();
+UserInterface::UserInterface(RectanglePro &rect) : m_rect(rect), m_text("") {
+  m_text.position = {.x = m_rect.x + (m_rect.width / 2.0f),
+                     .y = m_rect.y + (m_rect.height / 2.0f)};
+  m_text.rotation = m_rect.rotation;
+  m_text.CenterOrigin();
 }
 
-UserInterface::UserInterface(RectanglePro& rect, TextPro& text)
-	: m_rect(rect), m_text(text)
-{
-	m_text.position = {
-		.x = m_rect.x + (m_rect.width / 2.0f),
-		.y = m_rect.y + (m_rect.height / 2.0f)
-	};
-	m_text.rotation = m_rect.rotation;
+UserInterface::UserInterface(RectanglePro &rect, TextPro &text)
+    : m_rect(rect), m_text(text) {
+  m_text.position = {.x = m_rect.x + (m_rect.width / 2.0f),
+                     .y = m_rect.y + (m_rect.height / 2.0f)};
+  m_text.rotation = m_rect.rotation;
 
-	m_text.CenterOrigin();
+  m_text.CenterOrigin();
 }
 
-void UserInterface::Draw()
-{
-	//Font font = GetFontDefault();
-	UIUtils::DrawRectangleProRounded(m_rect);
-	UIUtils::DrawTextPro(m_text);
+void UserInterface::Draw() {
+  // Font font = GetFontDefault();
+  UIUtils::DrawRectangleProRounded(m_rect);
+  UIUtils::DrawTextPro(m_text);
 }
 
-void UserInterface::Update()
-{
-	this->Draw();
+void UserInterface::Update() { this->Draw(); }
+
+void UserInterface::HandleInput() {
+  // Input handling can be implemented in derived classes
 }
-
-void UserInterface::HandleInput()
-{
-	// Input handling can be implemented in derived classes
-}
-
-
-
--- src/userInterface/userInterface.h
+++ src/userInterface/userInterface.h
@@ -1,47 +1,43 @@
 #pragma once
 
-#include <string>
+#include <algorithm>
 #include <cmath>
-#include <algorithm> 
+#include <string>
 
-#include "raylib.h"
 #include "UIUtils.h"
-
+#include "raylib.h"
 
 class UserInterface {
 protected:
-	inline static const int DEFAULT_FONT_SIZE = 20; 
-	TextPro m_text;
-	RectanglePro m_rect;
+  inline static const int DEFAULT_FONT_SIZE = 20;
+  TextPro m_text;
+  RectanglePro m_rect;
 
-	UserInterface(RectanglePro& rect);
-	UserInterface(RectanglePro& rect, TextPro& text);
-
-
-
+  UserInterface(RectanglePro &rect);
+  UserInterface(RectanglePro &rect, TextPro &text);
 
 public:
-	virtual ~UserInterface() = default;
-	// Getters
-	[[nodiscard]] RectanglePro GetBounds() const { return m_rect; }
-	[[nodiscard]] float GetWidth() const { return m_rect.width; }
-	[[nodiscard]] float GetHeight() const { return m_rect.height; }
-	[[nodiscard]] float GetX() const { return m_rect.x; }
-	[[nodiscard]] float GetY() const { return m_rect.y; }
-	// Setters
-	void SetBounds(RectanglePro bounds) { m_rect = bounds; }
-	void SetPosition(float& x, float& y) { m_rect.x = x; m_rect.y = y; }
+  virtual ~UserInterface() = default;
+  // Getters
+  [[nodiscard]] RectanglePro GetBounds() const { return m_rect; }
+  [[nodiscard]] float GetWidth() const { return m_rect.width; }
+  [[nodiscard]] float GetHeight() const { return m_rect.height; }
+  [[nodiscard]] float GetX() const { return m_rect.x; }
+  [[nodiscard]] float GetY() const { return m_rect.y; }
+  // Setters
+  void SetBounds(RectanglePro bounds) { m_rect = bounds; }
+  void SetPosition(float &x, float &y) {
+    m_rect.x = x;
+    m_rect.y = y;
+  }
 
-	//bool isHovered() const { return m_isHovered; }
+  // bool isHovered() const { return m_isHovered; }
 
-	virtual void Draw();
-	virtual void Update();
-	virtual void HandleInput(); 
+  virtual void Draw();
+  virtual void Update();
+  virtual void HandleInput();
 
 private:
-	/*bool m_isHovered = false;
-	bool m_isPressed = false;*/
+  /*bool m_isHovered = false;
+  bool m_isPressed = false;*/
 };
-
-
-
