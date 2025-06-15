88e885b5699c2cd12bbe6fe75cf1cd9be122925f
We1l6.f <we1l6.f@gmail.com>
2025-06-15 16:31:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,6 @@
 #include "userInterface/button/button.h"
 
 int main() {
-  InitWindow(1920, 1080, "Hello World");
-  SetTargetFPS(60);
-  RectanglePro rect({ 1920 / 2, 1080 / 2 }, { 200, 50 }, 0.0f, 10.0f, BLUE);
-  TextPro text("CLICK");
-  Button button(rect, text);
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    button.Update();
-    EndDrawing();
-  }
-
   fmt::print("Hello, world!\n");
 
   spdlog::info("Welcome to spdlog!");
